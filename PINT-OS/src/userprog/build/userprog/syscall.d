e03c5987bfa5f3059d524a2a7976508cc7b6f7a7
RAVI SHANKAR <ravishankar98910@gmail.com>
2020-09-16 22:21:24 +0530

    added utils and virtual memory support vmware

---
+++ PINT-OS/src/userprog/build/userprog/syscall.d
@@ -0,0 +1,10 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+  ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+  ../../lib/kernel/stdio.h ../../lib/syscall-nr.h \
+  ../../threads/interrupt.h ../../threads/thread.h \
+  ../../lib/kernel/list.h ../../threads/synch.h ../../threads/vaddr.h \
+  ../../threads/loader.h ../../threads/init.h ../../userprog/process.h \
+  ../../filesys/file.h ../../filesys/off_t.h ../../filesys/filesys.h \
+  ../../threads/palloc.h ../../threads/malloc.h ../../devices/input.h \
+  ../../threads/synch.h
